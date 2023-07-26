ac206febe59890bd160ba4abf16c2deb61b052ef
deepaksharma33 <ds.fscoder@gmail.com>
2023-07-26 09:57:01 +0530

    Updates Controller spec

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -7,11 +7,30 @@ describe V1::TicketsController, type: :request do
     end
 
     it "responds with 200" do
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
 
     it "responds with no data message if there is missing data" do
       expect(response.body).to include("There is no ticket data")
+    end
+  end
+
+  describe "GET /show" do
+    before do
+      ticket = Ticket.new(request_number: Faker::Number.number(digits: 10),
+                          sequence_number: Faker::Number.number(digits: 10),
+                          request_type: Faker::String.random)
+      ticket.excavator = Excavator.new(crew_onsite: true)
+      ticket.datetime = Datetime.new(response_due_date_time: DateTime.now)
+      ticket.service_area = ServiceArea.new(primary_service_area_code: Faker::Code.asin,
+                                            additional_service_area_codes: Faker::Code.asin)
+      ticket.dig_site_info = DigSiteInfo.new(well_known_text: "POLYGON((-81.05047525138554 32.042681017283066,-81.12154306144413 32.08806865844325,-81.13390268058475 32.07206917625161))")
+      ticket.save
+      get "/v1/tickets/#{ticket.id}"
+    end
+
+    it "responds with 200" do
+      expect(response).to have_http_status(:ok)
     end
   end
 end
